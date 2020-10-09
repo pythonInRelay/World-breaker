ba151a9aa51385bbfe33b260d8a734de66df8cb2
Daniel <daniel.barber@edu.novia.fi>
2020-10-09 04:17:23 +0300

    Update transform.position

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float screenWidthInUnits = 16f;
+    
+    private void Update()
+    {
+        var mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        var transform1 = transform;
+        var paddlePos = new Vector2(mousePosInUnits, transform1.position.y);
+        transform1.position = paddlePos;
+    }
+}
